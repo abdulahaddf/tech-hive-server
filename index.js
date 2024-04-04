c1de4d42ea91050414cfb6e5094d581b8e89e858
abdulahaddf <abdulahad.df1@gmail.com>
2024-04-04 15:58:57 +0600

    cors origin

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173"
+}));
 app.use(express.json());
 
 
