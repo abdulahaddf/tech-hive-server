217541531800eb3a7ecd98db84a931b210284c1d
abdulahaddf <abdulahad.df1@gmail.com>
2024-04-06 16:50:49 +0600

    api

--- index.js
+++ index.js
@@ -5,18 +5,14 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Middleware
+// middleware
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 
-// Set custom headers
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow requests from all origins
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // Allow specified HTTP methods
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Allow specified headers
-  next();
-});
-
-// MongoDB URI
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@phonedb.ap54ge1.mongodb.net/?retryWrites=true&w=majority&appName=PhoneDB`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,20 +24,31 @@ const client = new MongoClient(uri, {
   },
 });
 
-
 async function run() {
   try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // client.connect((err) => {
+    //   if (err) {
+    //     console.error(err);
+    //     return;
+    //   }
+    // });
+
     const phoneCollection = client.db("phoneDB").collection("phones");
 
-    // Adding phone
+    //adding phone
     app.post("/addPhone", async (req, res) => {
       try {
         const newPhone = req.body;
+        console.log(newPhone);
         if (Object.keys(newPhone).length > 2) {
           newPhone.createdAt = new Date();
+          console.log(newPhone);
           const result = await phoneCollection.insertOne(newPhone);
+          console.log(result);
           res.send({ acknowledged: true, message: "Phone added successfully" });
         }
+        // res.send({ acknowledged: false, message: "Phone is not added" });
       } catch (error) {
         console.error("Error adding phone:", error);
         res.status(500).send("Error adding phone");
@@ -60,9 +67,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-// Define your routes here
+app.get("/", (req, res) => {
+  res.send("phone server is running");
+});
 
-// Start the server
 app.listen(port, () => {
-  console.log(`Phone server is running on port: ${port}`);
+  console.log(`phone Server is running on port: ${port}`);
 });
